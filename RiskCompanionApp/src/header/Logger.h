d66ec96dc4b736066247299fe521acc3de331a28
Suneil M <darthniax@gmail.com>
2013-10-16 23:57:36 -0400

    Added Logger() empty class for logging capability, gui sockets test worked so we are now linked...will update log by tonight.

---
+++ RiskCompanionApp/src/header/Logger.h
@@ -0,0 +1,22 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+/*****************************************************************************
+ *Class: Logger()
+ *Creator: Suneil Mistry
+ *Last Edit: 10-16-2013 10:36pm
+ *====================================
+ *Description: This class will log all data (movement, combat, etc). Used to
+ *      keep track of all movement (including manual overloads).
+ *
+ *Log:
+ *      10-16-2013-Suneil Mistry-> Initial Class creation.
+ *
+ ***************************************************************************/
+
+class Logger
+{
+public:
+    Logger();
+};
+
+#endif // LOGGER_H
